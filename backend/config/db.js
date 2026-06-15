066fc522b3a0758c766b5738e6cdf895b086de29
jenifer <jeniferbeula88@gmailcom>
2026-06-15 14:01:39 +0530

    Auto create database folder

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,15 @@
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const path = require('path');
+const fs = require('fs');
 
-const dbPath = path.join(__dirname, '../../database/habitflow.json');
+// Create database folder if it doesn't exist
+const dbDir = path.join(__dirname, '../../database');
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir, { recursive: true });
+}
+
+const dbPath = path.join(dbDir, 'habitflow.json');
 const adapter = new FileSync(dbPath);
 const db = low(adapter);
 
